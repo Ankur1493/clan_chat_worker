e8e3aaf966cd79897e404805083c80e0f9f33445
Ankur1493 <ankursharma1493@gmail.com>
2024-07-26 01:32:09 +0530

    ws init

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,45 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import WebSocket, { WebSocketServer } from 'ws';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
-const app: Express = express();
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.URI as string;
+
+const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(express.json())
-const port = process.env.PORT || 8000;
-const URI = process.env.URI as string
 
-app.get("/health", (req: Request, res: Response) => {
-  res.send("server is healthy");
-});
-
-mongoose.connect(URI)
+mongoose.connect(MONGO_URI)
   .then(() => {
-    app.listen(port, () => {
-      console.log(`[server]: Server is running at http://localhost:${port}`);
-    });
-  }).catch(err => {
-    console.log(err)
-  })
+    console.log('MongoDB connected');
 
+    const httpServer = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    const wss = new WebSocketServer({ server: httpServer });
+
+    wss.on('connection', function connection(ws) {
+      ws.on('error', console.error);
+
+      ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(data, { binary: isBinary });
+          }
+        });
+      });
+
+      ws.send('Hello! Message From Server!!');
+    });
+
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1)
+  });
 
